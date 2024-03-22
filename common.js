97c02b986183d6bf122a50e969a91ee56e7ec5b3
gaheeyoon <a5312559@gmail.com>
2024-03-22 20:50:29 +0900

    first commit

---
+++ common.js
@@ -0,0 +1,18 @@
+$('.img-container').on('mouseover', function() {
+    $(this).find('.img-content-hover').show();
+});
+
+$('.img-container').on('mouseout', function() {
+    $(this).find('.img-content-hover').hide();
+});
+
+
+
+// .img-container를 클릭하면 .prompt의 내용을 클립보드로 복사
+$('.img-container').on('click', function() {
+    var text = $(this).find('.img-content .prompt').text().trim();
+    alert(text);
+    var temp = $('<input>').val(text).appendTo('body').select();
+    document.execCommand('copy');
+    temp.remove();
+});
